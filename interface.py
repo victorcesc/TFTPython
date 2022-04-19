a33c79bb29a81aaaf3eb85904e10ab936877068d
Victor <victorcesconetto@gmail.com>
2022-04-19 17:16:04 -0300

    utilizando socket

--- interface.py
+++ interface.py
@@ -8,20 +8,20 @@ class TftpHandler:
 def __init__(self,ip,port):
     self.port = port
     self.ip = ip
+    cliente = socket(AF_INET, SOCK_DGRAM,17)
+    cliente.bind(('0.0.0.0',5555))
     
 
 def sendBlock(self,bytes):
-    # tem q ser servidor
     bloco = bytes
     try:
-        #self.cliente.sendto(bloco.encode(),(this.ip,this.port))
+        self.cliente.sendto(bloco.encode(),(this.ip,this.port))
     except Exception as e:
         print('Erro ao enviar')
 
-def receive(self):
-
+def receive(self,arq,arqlocal):
     try:
-        #dados,addr = self.cliente.recvfrom(512)
+        dados,addr = self.cliente.recvfrom(512)
     except Exception as e:
     if dados < 512 bytes:
         return "TERMINATE" 
